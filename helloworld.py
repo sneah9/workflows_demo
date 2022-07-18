5ec689e3f0899bc20025b91640bcec0e27365df5
sneah9 <102253815+sneah9@users.noreply.github.com>
2022-07-18 13:48:17 -0400

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,10 @@
+import sys
+
 def main():
-    print("Hello World!")
+    print("Hello World! From Python: " + str(sys.version_info))
+    if sys.version_info >= (3, 6) and sys.version_info < (3, 7):
+        # let's make this script fail for Python 3.6
+        raise Exception('Python version 3.6.x is unsupported!')
 
 if __name__ == "__main__":
     main()
